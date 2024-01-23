a980d38de51a87c19af70b393c22097633531e60
eniskrt <enskerti@gmail.com>
2024-01-23 20:05:13 +0300

    initial commit

---
+++ src/bootstrap-provider.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+
+const BootstrapProvider = ({children}) => {
+
+    useEffect(() => {
+        require("bootstrap/dist/js/bootstrap.bundle.min.js")
+    
+    }, [])
+    
+  return (
+    <>
+        {children}  
+    </>
+  )
+}
+
+export default BootstrapProvider
